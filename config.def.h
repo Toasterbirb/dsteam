8d8c0783626cb71fc2fb6ceec8b26a190034d36f
Toasterbirb <git.53jvg@aleeas.com>
2021-10-11 22:57:28 +0300

    Added line height patch back

--- config.def.h
+++ config.def.h
@@ -8,7 +8,7 @@ static char separator 		= ';';
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=15"
+	"Cantarell:size=15"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -21,6 +21,10 @@ static const char *colors[SchemeLast][2] = {
 static unsigned int lines 	= 30;
 static int limitlines 		= 1;  /* Prevent unnecessary lines, if there isn't enough items to fill those lines.
 									 Might cause a memory error in some cases if disabled. */
+
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 16;
+static unsigned int min_lineheight = 8;
 
 /*
  * Characters not considered part of a word while deleting words
